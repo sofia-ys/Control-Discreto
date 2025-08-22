b3c7ba0e7da3fe90b83cb03476e66f8b12f09bf2
sofia yanes sanchez <85056219+sofia-ys@users.noreply.github.com>
2025-08-22 15:02:25 -0300

    funcion de transferencia?

--- tp1/practica1.m
+++ tp1/practica1.m
@@ -1,1 +1,23 @@
-a = 2
+% datos del sistema
+Ra = 5;
+La = 0.7;
+Ki = 0.5;
+Ke = 0.5;
+Jm = 3;
+Bm = 0.01;
+N = 1000;
+Jl = 10000;
+Bl = 30;
+Jeq = Jm + (Jl/N^2);
+% Beq = 
+
+% funcion de transferencia
+s = tf("s");
+H = (1/N) * (1/( ...
+    (Ra/Ki) * Jeq *s^2 + (La/Ki) * Jeq * s^3 + Ke * s ...
+    ));
+
+P = pole(H);
+[Z,gain] = zero(H)
+
+% help zero
