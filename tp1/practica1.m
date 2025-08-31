897c1d2b3f0b3d3d699e19f724fc544e2b29f8a4
Toto51k <ttorea@itba.edu.ar>
2025-08-30 21:34:36 -0300

    Update practica1.m

--- tp1/practica1.m
+++ tp1/practica1.m
@@ -1,4 +1,10 @@
-%% datos del sistema
+%% Limpiar
+clear
+clc
+close all
+
+%% Datos del sistema
+format long
 Ra = 5;
 La = 0.7;
 Ki = 0.5;
@@ -11,50 +17,31 @@ Bl = 30;
 Jeq = Jm + (Jl/N^2);
 Beq = Bm + (Bl/N^2);
 
-%% a- Ecuación diferencial (lo tenemos hecho a mano)
+%% a- Ecuación diferencial (Hecho a mano)
 
-%% b- Funcion de transferencia (AHORA SI ESTÁ BIEN)
+%% b- Funcion de transferencia 
 s = tf("s");
 H = (1/N) * (1/( ...
-    (Ra/Ki) * Jeq *s^2 + (La/Ki) * Jeq * s^3 + Ke * s + (Ra/Ki) * Beq * s + (La/Ki) * Beq * s^2...
-    ));
+    (Ra/Ki) * Jeq *s^2 + (La/Ki) * Jeq * s^3 + Ke * s + (Ra/Ki) * Beq * s + (La/Ki) * Beq * s^2));
 
 %% c- Polos, ceros y diagrama de bode.
 
-P = pole(H); %serían los polos continuos
+P = pole(H); %Polos "continuos"
 [Z,gain] = zero(H);
 figure
 bode(H)
 hold on
 grid on
 
-% help zero
-
 %% d- Periodo de muestreo y transferencia del filtro anti-alias para discretizar el sistema.
-% Idealmente: fs>=2*fmax (Nyquist)
-% Regla: Elegir f entre 10*wbw y 20wbw (por ejemplo, 15)
-% wbw_rad = bandwidth(H); %%% SI TIRA NAN ES PORQUE TIENE GANANCIA INFINITA!
-%%% bandwidth() de MATLAB devuelve NaN xq el sistema tiene un polo en s=0. Este polo hace que la ganancia a frecuencia cero (DC) sea infinita
-%%% Necesitamos una métrica diferente pero conceptualmente similar para caracterizar la "velocidad" de nuestro sistema.
-%%% Frecuencia de Cruce de Ganancia
-%%% Es la frecuencia en la cual la magnitud del sistema en el diagrama de Bode es exactamente 1 (o = a 0dB)
-%%% Esta frecuencia nos dice "el punto a partir del cual el sistema atenúa las señales en lugar de amplificarlas"
-% fs_rad = 15*wbw_rad;
-% fs_Hz = fs_rad / (2*pi());
-% Período de muestreo
-% T = 1 / fs_Hz;
-% Usamos la función margin(), que nos da esta frecuencia directamente.
-[Gm, Pm, Wcg, Wcp] = margin(H); % Wcg es la frecuencia que buscamos (en rad/s)
-% Repito lo anterior pero con w_gc
+[Gm, Pm, Wcg, Wcp] = margin(H); % Wcg es la frecuencia buscada (en rad/s)
 fs_rad = 15 * Wcg; % Frecuencia de muestreo en rad/s
 fs_Hz = fs_rad / (2*pi);
 T = 1 / fs_Hz;
 
-% Filtro anti-alias
-% Tipo de filtro: Es siempre un filtro pasa-bajos.
-% Sea la frecuencia de corte wc Es la mitad de la frecuencia de muestreo.
+% Filtro anti-alias: Pasa bajos
 wc = fs_rad/2;
-% El filtro anti-alias más simple es uno de primer orden, cuya función de transferencia es:
+% De primer orden con ésta transferencia:
 H_filtro = (wc / (s + wc));
 
 %% e- Discretizar el sistema y obtener ΘL(z)/En(z), suponiendo ZOH en la entrada 
