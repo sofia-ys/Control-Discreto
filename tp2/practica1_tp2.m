1939672a731b63e4896682ac5b5ed7c30f8fbeff
Toto51k <ttorea@itba.edu.ar>
2025-09-17 01:00:25 -0300

    Update practica1_tp2.m
    Base importante para terminar lo que falta

--- tp2/practica1_tp2.m
+++ tp2/practica1_tp2.m
@@ -5,13 +5,16 @@ close all
 %% a- Proponer controladores PID discretos que cumpla las siguientes especificaciones (tiene que ser un controlador por cada sub-item):
 % I. Error estacionario al escalón nulo y sobrepico = 20%
 % II. Error estacionario al escalón nulo y sin sobrepico.
+
 % Se carga la función de transferencia discreta 
 numerador_p1 = [0 1.587098114499472e-5 2.404465224969516e-5 6.336036425624988e-7];
 denominador_p1 = [1 -1.978426373061843 0.978785811198180 -3.594381363373757e-4];
 T_p1 = 1.109817844007972;
 Gz_p1 = tf(numerador_p1, denominador_p1, T_p1);
+
 % Se lanza la herramienta
 % controlSystemDesigner(Gz_p1);
+
 % Se elige un tiempo suficientemente grande como para reflejar que el
 % sistema efectivamente alcanza el estado estacionario.
 Tfinal = 20000;
@@ -23,6 +26,7 @@ Cz_p1_2 = pid(3.1, 0, 0, T_p1/10, 'Ts', T_p1); %Kp Ki Kd
 Tz_p1_1 = feedback(series(Cz_p1_1,Gz_p1),1);
 % Salida II
 Tz_p1_2 = feedback(series(Cz_p1_2,Gz_p1),1);
+
 % Gráficos e información
 [y1,t1] = step(Tz_p1_1,Tfinal);
 [y2,t2] = step(Tz_p1_2,Tfinal);
@@ -35,7 +39,8 @@ figure
 hold on
 step(Tz_p1_2);
 title('Respuesta al escalón 0% sobrepico')
-info_2 = stepinfo(y2,t2)
+info_2 = stepinfo(y2,t2);
+
 % Error de estado estacionario al escalón
 ess_1 = 1 - y1(end);
 ess_2 = 1 - y2(end);
@@ -52,25 +57,27 @@ Ts_p1_1=d2c(Tz_p1_1, 'tustin');
 % Planta compensada II convertida a W
 Ts_p1_2=d2c(Tz_p1_2, 'tustin');
 
-%%% REVISAR!!!
+%Gráficos Bode
 figure
 hold on
 bodeplot(Gs_p1);
 bodeplot(series(Cz_p1_1, Gz_p1));
-legend('Planta Original', 'Planta + Controlador');
+legend('Planta Original', 'Planta + Controlador I');
+title('Bode: Planta original y compensador I')
 figure 
 hold on
 bodeplot(Gs_p1);
 bodeplot(series(Cz_p1_2, Gz_p1));
 legend('Planta Original', 'Planta + Controlador');
+title('Bode: Planta original y compensador II')
 grid on
 
 % Se calculan los márgenes de ganancia y fase
 [Gm_planta, Pm_planta] = margin(Gs_p1);
 [Gm_compensado_1, Pm_compensado_1] = margin(series(Cz_p1_1, Gz_p1));
 [Gm_compensado_2, Pm_compensado_2] = margin(series(Cz_p1_2, Gz_p1));
-%isstable(Ts_p1_1)
-%isstable(Ts_p1_2)
+isstable(Tz_p1_1);
+isstable(Tz_p1_2);
  
 fprintf('Planta Original: Margen de Ganancia = %f dB, Margen de Fase = %f deg\n', 20*log10(Gm_planta), Pm_planta);
 fprintf('Sistema Compensado I : Margen de Ganancia = %f dB, Margen de Fase = %f deg\n', 20*log10(Gm_compensado_1), Pm_compensado_1);
